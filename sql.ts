3b9cc59378673a3d6e8364181be3e2a6fdac8334
ikepu-tp <postmaster@licsss.jp>
2024-09-18 23:11:20 +0900

    add dd

--- sql.ts
+++ sql.ts
@@ -15,16 +15,39 @@ class SQL {
 		if (this.spreadSheetId !== null) this.getSpreadById(spreadSheetId as unknown as string);
 	}
 
-	getSpreadById(spreadSheetId: string) {
+	/**
+	 * Get SpreadSheet by Id
+	 *
+	 * @param {string} spreadSheetId
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
+	getSpreadById(spreadSheetId: string): this {
 		this.spreadSheetId = spreadSheetId;
 		this.spreadSheet = SpreadsheetApp.openById(spreadSheetId);
+		return this;
 	}
 
-	getSpreadByUrl(spreadSheetUrl: string) {
+	/**
+	 * Get SpreadSheet by Url
+	 *
+	 * @param {string} spreadSheetUrl
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
+	getSpreadByUrl(spreadSheetUrl: string): this {
 		this.spreadSheet = SpreadsheetApp.openByUrl(spreadSheetUrl);
 		this.spreadSheetId = this.spreadSheet.getId();
+		return this;
 	}
 
+	/**
+	 * Set Sheet by Name
+	 *
+	 * @param {string} sheetName
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
 	setSheetByName(sheetName: string): this {
 		const sheet = this.spreadSheet.getSheetByName(sheetName);
 		if (!sheet) throw new Error(`Sheet ${sheetName} not found`);
@@ -32,35 +55,89 @@ class SQL {
 		return this;
 	}
 
+	/**
+	 * Set Columns to Select
+	 *
+	 * @param {string[]} columns
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
 	select(columns: string[]): this {
 		return this;
 	}
 
+	/**
+	 * Set Where Clause
+	 *
+	 * @param {RecordType} wheres
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
 	where(wheres: RecordType): this {
 		return this;
 	}
 
+	/**
+	 * Set Or Where Clause
+	 *
+	 * @param {RecordType} wheres
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
 	orWhere(wheres: RecordType): this {
 		return this;
 	}
 
+	/**
+	 * Set Where In Clause
+	 *
+	 * @param {string} column
+	 * @param {((string | number)[])} values
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
 	whereIn(column: string, values: (string | number)[]): this {
 		return this;
 	}
 
-	get(): any[] {
+	/**
+	 * Get Records
+	 *
+	 * @return {*}  {RecordType[]}
+	 * @memberof SQL
+	 */
+	get(): RecordType[] {
 		return [];
 	}
 
+	/**
+	 * Get First Record
+	 *
+	 * @return {*}  {*}
+	 * @memberof SQL
+	 */
 	first(): any {
 		this.rowId = 0;
 		return {};
 	}
 
+	/**
+	 * Fill Record
+	 *
+	 * @param {RecordType} data
+	 * @return {*}  {this}
+	 * @memberof SQL
+	 */
 	fill(data: RecordType): this {
 		return this;
 	}
 
+	/**
+	 * Save Record
+	 *
+	 * @return {*}  {boolean}
+	 * @memberof SQL
+	 */
 	save(): boolean {
 		if (this.rowId) {
 			// update
